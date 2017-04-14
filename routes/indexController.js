96f66d70bc7f106c301b0150a5f1396bff656cd4
tengfeis <tengfeis@jumei.com>
2017-04-14 17:17:50 +0800

    radme

--- routes/indexController.js
+++ routes/indexController.js
@@ -3,7 +3,7 @@ var mongodb = require('mongodb').MongoClient;
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Vue-' });
+    res.render('index', { title: 'Vue-asdfasd' });
 });
 // router.post('/*', function(req, res, next) {
 //     mongodb.connect('mongodb://localhost:27017/blog',function (err,db) {
