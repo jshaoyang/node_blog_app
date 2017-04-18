06d9dbdb5011905203a14faea738626615fa0081
tengfeis <tengfeis@jumei.com>
2017-04-18 10:25:09 +0800

    port

--- util/tool.js
+++ util/tool.js
@@ -2,8 +2,8 @@ var mian = {};
 mian.normalize = function normalizeNumber (val) {
     var port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return val;
+    if (!isNaN(port)) {
+        return port;
     }
 
     if (port >= 0) {
